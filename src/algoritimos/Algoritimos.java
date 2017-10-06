afbc9d9485bdca28131f6bc9fb4cbaef9dcef160
Notebook <Notebook@DESKTOP-7DRAUFD>
2017-10-05 21:20:17 -0300

    Foram adicionadas algumas classes

--- src/algoritimos/Algoritimos.java
+++ src/algoritimos/Algoritimos.java
@@ -3,8 +3,25 @@ package algoritimos;
 
 public class Algoritimos {
 
-    public static void main(String[] args) {
-
+    public static void Quadrado (double n){
+        double r = n * n;
     }
+    
+    public static double metade(double a){
+        double r = a/2;
+        return r;
+    }
+    
+    public static void main(String[] args) {
+        Quadrado(15);
+        
+        double raiz;
+        raiz = Math.sqrt(15);
+        double resultado;
+        resultado = metade(15);
+        
+        System.out.println("A raiz é " + raiz);
+    }
+    
     
 }
